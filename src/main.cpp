dace2fa93f69758c31380a86019593d0492bd86b
vcvycy <352871242@qq.com>
2018-05-18 10:52:31 +0800

    .

--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,11 @@
 
 #include<sys/time.h>
 #include "GloFun.h"
-//test函数：测试效果
-// (1)从../img/first/  读取108张图片插入数据库
-// (2)从../img/second/ 读取108张图片在数据库中查询
-#define TEST_MODE
-int test(IrisManager &mgr){
-  printf("=============准确率测试==============\n");
-  //插入
-#define IMGSZ 108
-  std::string file;
-  char filename[50];
-  int id[110];
-  
-  for (int i=1;i<=IMGSZ;i++){
-    sprintf(filename,"../img/first/%03d_1_1.bmp",i);
-    file=std::string(filename);
-    id[i]=i+173;
-    if (id[i]<0) printf("[!]失败%s\n",filename);else printf("[*]图片%s插入成功,id %d\n",filename,id[i]);
-  }
-  //查询
-  int cnt=0;
-  for (int i=1;i<=IMGSZ;i++){
-    float sco;
-    sprintf(filename,"../img/second/%03d_1_2.bmp",i);
-    printf("query %s\n",filename);
-    file=std::string(filename);
-    //TIME_START
-    int match_id=mgr.query(file,&sco);
-    //TIME_SHOW
-    if (match_id==id[i]){
-      printf("[*]匹配成功%d -> %d sco=%f\n",i,match_id,sco);
-      cnt++;
-    }else printf("[*]faile%d -> %d sco=%f\n",i,match_id,sco);
-  }
-  printf("===成功率%d/%d===\n",cnt,IMGSZ);
-}
-int main (int argc, char * argv[]){
-    //配置文件路径，只需要传入文件夹，会自动读取文件夹中的conf.ini文件。
-    char* confPath=(char*)"../conf/";
-    if (argc>2) confPath=argv[1];
-    
-    //
-    IrisManager mgr(confPath);//参数为配置文件的路径
-    mgr.showConf();
-#ifdef TEST_MODE
-    test(mgr);return 0;
-#endif
+int main (int argc, char * argv[]){ 
+    //(1) load config file[conf
+    char* configPath=(char*)"../conf/"; 
+    IrisManager mgr(configPath); //参数为配置文件的路径
+    mgr.showConf(); 
     //
     int  id;
     char op[10];
